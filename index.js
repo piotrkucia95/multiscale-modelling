0c36a90fe0878b6d60998447539e6c0b614e68b5
Piotr Kucia <piotrkucia95@gmail.com>
2019-10-17 12:47:09 +0200

    Node App Setup

---
+++ index.js
@@ -0,0 +1,79 @@
+const electron  = require('electron');
+const url       = require('url');
+const path      = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+// SET ENV
+// process.env.NODE_ENV = 'production';
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('close', function() {
+        app.quit();
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Microstructure',
+                submenu: [
+                    {
+                        label: 'Import',
+                        click() {
+                            console.log('Import placeholder');
+                        }
+                    }, {
+                        label: 'Export',
+                        click() {
+                            console.log('Export placeholder');
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+];
+
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools items if not in production
+if (process.env.NODE_ENV != 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
